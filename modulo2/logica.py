1289516d978cc02030518fe65043ac9beace20f0
keslly em vocacao <kesllyagatha@gmail.com>
2025-08-31 20:03:01 -0300

    Update logica.py
    expressões matemáticas usando operadoras aritméticas

--- modulo2/logica.py
+++ modulo2/logica.py
@@ -1,1 +1,13 @@
+idade = int(input("Digite a sua idade: "))
 
+# Estrutura condicional para classificar a idade
+if idade < 0:
+    print("Idade inválida. A idade não pode ser um número negativo.")
+elif idade <= 12:
+    print("Você é uma Criança.")
+elif idade <= 17:
+    print("Você é um Adolescente.")
+elif idade <= 59:
+    print("Você é um Adulto.")
+else:
+    print("Você é um Idoso.")
