8ce414072ea19bb099d8073bc78c7c57012f6d27
keslly em vocacao <kesllyagatha@gmail.com>
2025-09-02 20:59:54 -0300

    Create Django.py

---
+++ modulo_13/Django.py
@@ -0,0 +1,23 @@
+# meuprojeto/settings.py
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'produtos', # Adicione esta linha
+]
+# produtos/models.py
+
+from django.db import models
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade = models.IntegerField()
+    
+    def __str__(self):
+        return self.nome
